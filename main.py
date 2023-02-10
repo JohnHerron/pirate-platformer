89b7394198fbeaf30ca80eda7d3c52ea6ef3dee6
John Herron <herroncjohn@gmail.com>
2023-02-10 01:00:17 -0800

    Tile and Level class setups

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from settings import *
+from tiles import Tile
+from level import Level
+
+# set up pygame
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+test_tile = pygame.sprite.Group(Tile((100,100),200))
+level = Level(level_map, screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        screen.fill('black')
+        level.run()
+
+        pygame.display.update()
+        clock.tick(60)
